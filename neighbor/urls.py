1dbf17bf27c75c45747c780d7a46251ca9310221
juru-juliette <kangabejuliette@gmail.com>
2019-11-01 19:11:34 +0200

    creating forms

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -4,4 +4,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns=[
     url('^$',views.home,name = 'home'),
+    url(r'^accounts/profile/(\d+)', views.profile, name='profile'),
+    url(r'^accounts/edit_profile/', views.edit_profile, name='edit_profile'),
 ]
--- neighbor/views.py
+++ neighbor/views.py
@@ -15,3 +15,16 @@ def profile(request,id):
    user = Profile.objects.get(username__id=id)
    projects = Project.objects.all()
    return render(request, "NG/profile.html", {"current_user":current_user,"projects":projects,"user":user,"user_object":user_object})
+
+@login_required(login_url='/accounts/login/')
+def edit_profile(request):
+   current_user=request.user
+   user_edit = Profile.objects.get(username__id=current_user.id)
+   if request.method =='POST':
+       form=ProfileForm(request.POST,request.FILES,instance=request.user.profile)
+       if form.is_valid():
+           form.save()
+           return redirect('profile')
+   else:
+       form=ProfileForm(instance=request.user.profile)
+   return render(request,'AW/edit_profile.html',locals())
