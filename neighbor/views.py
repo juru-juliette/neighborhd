4734eb5e221e0fdbe206d5ca07409d2198b6adb6
juru-juliette <kangabejuliette@gmail.com>
2019-11-01 14:23:11 +0200

    adding navbar

--- neighbor/views.py
+++ neighbor/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,HttpResponseRedirect
 # Create your views here.
+def home(request):
+    title='Neighbor'
+    return render(request,'NG/home.html',{'title':title})
