55ba91475ab1113fcb8421661bec4c359df4b305
juru-juliette <kangabejuliette@gmail.com>
2019-11-04 10:09:13 +0200

    creating superuser

--- neighbor/admin.py
+++ neighbor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Neighborhood)
+admin.site.register(Business)
+admin.site.register(Post)
+
